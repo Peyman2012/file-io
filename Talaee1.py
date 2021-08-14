c21224840a88f6bfd894a65c8dee3b155e0d846a
Peyman2012 <88220773+Peyman2012@users.noreply.github.com>
2021-08-15 00:02:28 +0430

    Add files via upload

--- Talaee1.py
+++ Talaee1.py
@@ -1,9 +1,61 @@
-number = int(input("Enter a number Mani: "))
-c = number % 10
-number = number // 10
-b = number % 10
-number = number // 10
-a = number % 10
-inverse = c * 100 + b * 10 + a
+#This code was written by one of my students:
+#Abtin Torabi
+from turtle import *
+speed(0)
+penup()
+goto(-100,0)
+pendown()
 
-print("Your inverse number is ", inverse)
+for i in range(4) :
+    fd(200)
+    lt(90)
+    
+penup()
+goto(103,200)
+pendown()
+lt(139)
+fd(130)
+lt(80)
+fd(136)
+
+penup()
+goto(61,238)
+pendown()
+rt(129)
+fd(87)
+lt(90)
+fd(30)
+lt(90)
+fd(65)
+
+penup()
+goto(0,0)
+pendown()
+lt(90)
+for a in range(2) :
+    fd(40)
+    lt(90)
+    fd(100)
+    lt(90)
+    
+penup()
+goto(-60,150)
+pendown()
+lt(90)
+for c in range(4) :
+    lt(90)
+    for f in range(4) :
+        fd(20)
+        lt(90)
+rt(90)
+penup()
+goto(6,210)
+pendown()
+for q in range(72) :
+    fd(2)
+    lt(5)
+lt(90)
+fd(47)
+penup()
+goto(-100,0)
+pendown()
