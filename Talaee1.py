817520d19db02b6573e298ecb5e29d9b6ffa5af7
Peyman2012 <88220773+Peyman2012@users.noreply.github.com>
2021-08-15 00:03:21 +0430

    Add files via upload

--- Talaee1.py
+++ Talaee1.py
@@ -1,61 +1,30 @@
-#This code was written by one of my students:
-#Abtin Torabi
-from turtle import *
-speed(0)
-penup()
-goto(-100,0)
-pendown()
+# this program was written by one of my students , "Parham Talebi"
 
-for i in range(4) :
-    fd(200)
-    lt(90)
-    
-penup()
-goto(103,200)
-pendown()
-lt(139)
-fd(130)
-lt(80)
-fd(136)
+from random import *
 
-penup()
-goto(61,238)
-pendown()
-rt(129)
-fd(87)
-lt(90)
-fd(30)
-lt(90)
-fd(65)
+print(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.")
+print(".                                                                     .")
+print(".                                                                     .")
+print(".                                                                     .")
+print(".                                                                     .")
+print(".      Hello...!      I AM YOUR SMART PASSWORD GENERATOR   :)         .")
+print(".                                                                     .")
+print(".                                                                     .")
+print(".                                                                     .")
+print(".                                                                     .")
+print(".                                                                     .")
+print(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.")
 
-penup()
-goto(0,0)
-pendown()
-lt(90)
-for a in range(2) :
-    fd(40)
-    lt(90)
-    fd(100)
-    lt(90)
-    
-penup()
-goto(-60,150)
-pendown()
-lt(90)
-for c in range(4) :
-    lt(90)
-    for f in range(4) :
-        fd(20)
-        lt(90)
-rt(90)
-penup()
-goto(6,210)
-pendown()
-for q in range(72) :
-    fd(2)
-    lt(5)
-lt(90)
-fd(47)
-penup()
-goto(-100,0)
-pendown()
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+         'w', 'x', 'y', 'z',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+         'W', 'X', 'Y', 'Z',
+         '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', '=', '!', '@', '$', '%', '^', '&', '*']
+print("")
+length = int(input("Enter the length of password : "))
+print("")
+password = ""
+for i in range(length):
+    x = randint(0, len(chars) - 1)
+    password = password + chars[x]
+print(password)
